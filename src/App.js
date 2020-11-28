eb81308315bc144b2141540ae431ae812f56d048
C0ldHands <48500351+C0ldHands@users.noreply.github.com>
2020-11-27 19:26:30 -0500

    Started bookSearch, having issues rerendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Search from './search';
+import PrintType from './printType';
+import BookType from './bookType';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      searchTerm: "henry",
+      printType: "all",
+      bookType: ""
+    };
+  }
+
+  componentDidMount() {
+    const apiKey = 'APIKey';
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.searchTerm}&printType=${this.state.printType}&key=${apiKey}`;
+    if (this.state.bookType !== ""){
+      url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.searchTerm}&printType=${this.state.printType}&filter=${this.state.bookType}&key=${apiKey}`
+    }
+    console.log(url);
+    fetch(url)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        // console.log(data.items[0].volumeInfo.title);
+        let tempResults = [];
+        for (let i =0;i<data.items.length;i++){
+          tempResults.push(data.items[i].volumeInfo.title)
+        }
+        this.setState({
+          results: tempResults
+        });
+        // console.log(this.state.results)
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+
+  }
+
+  setSearchTerm = (event) =>{
+    event.preventDefault()
+    console.log(event.target.value)
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+  setPrintType = (event)=>{
+    // console.log(event.target.value)
+    this.setState({
+      printType: event.target.value
+    });
+  }
+
+  setBookType = (event) =>{
+    // console.log(event.target.value)
+    this.setState({
+      bookType: event.target.value
+    });
+  }
+
+  render(){
+    return (
+      <main className='App'>
+        <h1>Google Book Search</h1>
+        <Search handleSearchTerm = {this.setSearchTerm}/>
+        <br/>
+        <PrintType handlePrintType = {this.setPrintType}/>
+        <BookType handleBookType = {this.setBookType}/>
+        <br/>
+        <ul>
+          {this.state.results.map((title,i)=>{
+            return <li key = {i}>{title}</li>
+          })}
+        </ul>
+      </main>
+    );
+  }
 }
 
 export default App;
